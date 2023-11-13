0cafc70073766db1a542e89037e3a7c7f632ec08
OscarB <obiondi99@gmail.com>
2023-11-13 16:06:36 -0500

    needs css on home

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <main>
         <h1> Home Page </h1>
-        <Link href="/blankTab" >Link2</Link>
+        <Link href="/blankTab" >New Tab Project</Link>
+        <Link href="/portfolio" >Portfolio Project</Link>
+        <Link href="/tictactoe" >Tictactoe Project</Link>
     </main>
   )
 }
