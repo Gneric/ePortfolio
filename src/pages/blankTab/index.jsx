ac201b4710a82c29ab7042cd59a3fd8069ff369a
Gneric <obiondi99@gmail.com>
2024-05-10 11:28:22 -0500

    blankTab completado

--- src/pages/blankTab/index.jsx
+++ src/pages/blankTab/index.jsx
@@ -6,27 +6,26 @@ import LinkLi from '@/pages/blankTab/components/LinkLi'
 
 var initialLinks = [
   {
-    "key": ":ytb",
+    "key": Math.random() * 10 ** 17,
+    "prefix": ":you",
     "url": "https://www.youtube.com/",
     "placeholder": "Youtube"
   },
   {
-    "key": ":wsp",
+    "key": Math.random()  * 10 ** 17,
+    "prefix": ":wha",
     "url": "https://web.whatsapp.com/",
     "placeholder": "Whatsapp"
   },
   {
-    "key": ":gth",
+    "key": Math.random()  * 10 ** 17,
+    "prefix": ":gth",
     "url": "http://github.com",
     "placeholder": "Github"
   },
   {
-    "key": ":igm",
-    "url": "http://instagram.com",
-    "placeholder": "Instagram"
-  },
-  {
-    "key": ":nya",
+    "key": Math.random()  * 10 ** 17,
+    "prefix": ":nya",
     "url": "https://nyafilmer.gg/",
     "placeholder": "Nyafilmer"
   }
@@ -38,6 +37,7 @@ export default function BlankPage () {
   const [links, setLinks] = useState([])
   const [placeholder, setPlaceholder] = useState('')
   const [url, setUrl] = useState('')
+  const [itemKey, setItemKey] = useState('')
 
 
   useEffect(() => {
@@ -47,57 +47,117 @@ export default function BlankPage () {
   }, [])
 
   useEffect(() => {
+    console.log('Change on links detected, changing local storage')
     localStorage.setItem('storedLinks', JSON.stringify(links))
   }, [links])
 
-  const handleAddLink = () => {
-    if( links.find( item => item.url === url ) ) {return}
-    else {
-      let key = `:${placeholder.substring(0, 3)}`
-      setLinks([...links, { "key": key, "url": url, "placeholder": placeholder }])
+  const handleSubmitModal = () => {
+    if ( editMode ) {
+      const itemFound = links.find((i) => i.key == itemKey )
+      itemFound.url = url
+      itemFound.placeholder = placeholder
       setPlaceholder('')
       setUrl('')
+      setItemKey('')
+    } else {
+      if( links.find((i) => i.url == url ) == null ) {
+        console.log('Adding link')
+        let prefix = `:${placeholder.substring(0, 3)}`
+        setLinks([...links, { "key": Math.random() * 10 ** 17, "prefix": prefix, "url": url, "placeholder": placeholder }])
+        setPlaceholder('')
+        setUrl('')
+      }
     }
   }
 
-  const handleRemoveLink = ({ url }) => {
-    console.log('Deleting ' + url)
-    const newTodos = links.filter((i) => i.url !== url)
+  function handleOnOpenChange() {
+    if( editMode ) {
+      setPlaceholder('')
+      setUrl('')
+    }
+    onOpenChange()
+  }
+
+  const handleRemoveLink = ({ itemKey }) => {
+    const newTodos = links.filter((i) => i.key !== itemKey)
+    console.log(newTodos)
     setLinks(newTodos)
+  }
+
+  const handleEditLink = ({ itemKey }) => {
+    const foundLink = links.find((i) => i.key === itemKey)
+    setPlaceholder(foundLink.placeholder)
+    setUrl(foundLink.url)
+    setItemKey(itemKey)
+    onOpen()
   }
 
   const generatedlinks = links.map( item => {
       return <LinkLi
-          key={Math.random()}
-          prefix={item.key}
-          value={item.placeholder}
+          key={item.key}
+          itemKey={item.key}
+          prefix={item.prefix}
+          placeholder={item.placeholder}
           url={item.url}
           editMode={editMode}
           handleRemoveLink={handleRemoveLink}
+          handleEditLink={handleEditLink}
       />
   })
 
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen" style={{ backgroundColor: '#171928' }} >
-      <div className='font-meiryo text-8xl font-bold' style={{ color: '#f4f3f7' }}> いらっしゃいませ </div>
+      <div className='font-meiryo text-4xl xs:text-4xl md:text-6xl lg:text-8xl font-bold' style={{ color: '#f4f3f7' }}>
+        おいらっしゃいませ 
+      </div>
       <ul className='flex flex-wrap pt-12 w-8/12 justify-evenly p-0'>
         {generatedlinks}
       </ul>
       <div className='w-screen absolute bottom-5 text-center' >
+
         <Button className='font-bold m-2' onPress={ () => setEditMode(!editMode) } color="warning">Edit Mode</Button>
-        <Button className='font-bold m-2' onPress={onOpen} color="success">Add Link</Button>
-        <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center">
+        <Button className='font-bold m-2' onPress={onOpen} color="success" isDisabled={ editMode ? true : false } >Add Link</Button>
+
+
+        <Modal placement="bottom" isOpen={isOpen} onOpenChange={handleOnOpenChange}>
           <ModalContent className='bg-slate-800' >
             {(onClose) => (
               <>
                 <ModalHeader className="flex flex-col gap-1 font-bold text-white">Add Link</ModalHeader>
                 <ModalBody>
-                  <Input autoFocus required label="Name" placeholder="Enter the name" variant="faded" value={placeholder} onChange={(e) => setPlaceholder(e.target.value)} />
-                  <Input required label="URL" placeholder="Enter the URL" variant="faded" value={url} onChange={(e) => setUrl(e.target.value)} />
+                  <Input
+                    classNames={{
+                      inputWrapper: 'cursor-text',
+                      input: 'cursor-text'
+                    }}
+                    autoFocus
+                    required 
+                    label="Name"
+                    placeholder="Enter the name" 
+                    variant="flat" 
+                    value={placeholder} 
+                    onChange={(e) => setPlaceholder(e.target.value)} 
+                  />
+                  <Input
+                    classNames={{
+                      inputWrapper: 'cursor-text',
+                      input: 'cursor-text'
+                    }}
+                    required 
+                    label="URL" 
+                    placeholder="Enter the URL" 
+                    variant="flat" 
+                    value={url} onChange={(e) => setUrl(e.target.value)} 
+                  />
                 </ModalBody>
                 <ModalFooter>
-                  <Button color="primary" onPress={() => { onClose(); handleAddLink() }}>
-                    Add
+                  <Button 
+                    color="primary" 
+                    onPress={() => { 
+                      handleSubmitModal()
+                      onClose() 
+                    }}>
+                      { editMode ? 'Edit' : 'Add' } 
                   </Button>
                 </ModalFooter>
               </>
